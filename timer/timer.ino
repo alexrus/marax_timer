faf0dd31034f2e845c2d47889bf650cc63265220
Alex Rus <alex.rus@heits.digital>
2020-10-25 17:38:27 +0200

    show only timer when pump is active

--- timer/timer.ino
+++ timer/timer.ino
@@ -59,7 +59,6 @@ void loop() {
 
 void getMachineInput() {
   while (mySerial.available() ) {
-//    displayOn = true;
     serialUpdateMillis = millis();
     rc = mySerial.read();
 
@@ -89,7 +88,6 @@ void detectChanges() {
     timerStartMillis = millis();
     timerStarted = true;
     displayOn = true;
-    display.invertDisplay(true);
     Serial.println("Start pump");
   }
   if (timerStarted && digitalRead(PUMP_PIN)) {
@@ -133,57 +131,63 @@ String getTimer() {
 void updateDisplay() {
   display.clearDisplay();
   if (displayOn) {
-    // draw line
-    display.drawLine(74, 0, 74, 63, SSD1306_WHITE);
-    // draw time seconds
-    display.setTextSize(4);
-    display.setCursor(display.width() / 2 - 1 + 17, 20);
-    display.print(getTimer());
-    // draw machine state C/S
-    if (receivedChars[0] ) {
-      display.setTextSize(2);
-      display.setCursor(1, 1);
-      if (String(receivedChars[0]) == "C") {
+    if (timerStarted) {
+      display.setTextSize(7);
+      display.setCursor(25, 8);
+      display.print(getTimer());
+    } else {
+      // draw line
+      display.drawLine(74, 0, 74, 63, SSD1306_WHITE);
+      // draw time seconds
+      display.setTextSize(4);
+      display.setCursor(display.width() / 2 - 1 + 17, 20);
+      display.print(getTimer());
+      // draw machine state C/S
+      if (receivedChars[0] ) {
+        display.setTextSize(2);
+        display.setCursor(1, 1);
+        if (String(receivedChars[0]) == "C") {
+          display.print("C");
+        } else if (String(receivedChars[0]) == "V") {
+          display.print("S");
+        } else {
+          display.print("X");
+        }
+      }
+      // draw fill circle if heating on
+      if (String(receivedChars[23]) == "1") {
+        display.fillCircle(45, 7, 6, SSD1306_WHITE);
+      }
+      // draw empty circle if heating off
+      if (String(receivedChars[23]) == "0") {
+        display.drawCircle(45, 7, 6, SSD1306_WHITE);
+      }
+      // draw temperature
+      if (receivedChars[14] && receivedChars[15] && receivedChars[16]) {
+        display.setTextSize(3);
+        display.setCursor(1, 20);
+        if (String(receivedChars[14]) != "0") {
+          display.print(String(receivedChars[14]));
+        }
+        display.print(String(receivedChars[15]));
+        display.print(String(receivedChars[16]));
+        display.print((char)247);
+        if (String(receivedChars[14]) == "0") {
+          display.print("C");
+        }
+      }
+      // draw steam temperature
+      if (receivedChars[6] && receivedChars[7] && receivedChars[8]) {
+        display.setTextSize(2);
+        display.setCursor(1, 48);
+        if (String(receivedChars[6]) != "0") {
+          display.print(String(receivedChars[6]));
+        }
+        display.print(String(receivedChars[7]));
+        display.print(String(receivedChars[8]));
+        display.print((char)247);
         display.print("C");
-      } else if (String(receivedChars[0]) == "V") {
-        display.print("S");
-      } else {
-        display.print("X");
       }
-    }
-    // draw fill circle if heating on
-    if (String(receivedChars[23]) == "1") {
-      display.fillCircle(45, 7, 6, SSD1306_WHITE);
-    }
-    // draw empty circle if heating off
-    if (String(receivedChars[23]) == "0") {
-      display.drawCircle(45, 7, 6, SSD1306_WHITE);
-    }
-    // draw temperature
-    if (receivedChars[14] && receivedChars[15] && receivedChars[16]) {
-      display.setTextSize(3);
-      display.setCursor(1, 20);
-      if (String(receivedChars[14]) != "0") {
-        display.print(String(receivedChars[14]));
-      }
-      display.print(String(receivedChars[15]));
-      display.print(String(receivedChars[16]));
-      display.print((char)247);
-      if (String(receivedChars[14]) == "0") {
-        display.print("C");
-      }
-    }
-    // draw steam temperature
-    if (receivedChars[6] && receivedChars[7] && receivedChars[8]) {
-      display.setTextSize(2);
-      display.setCursor(1, 48);
-      if (String(receivedChars[6]) != "0") {
-        display.print(String(receivedChars[6]));
-      }
-      display.print(String(receivedChars[7]));
-      display.print(String(receivedChars[8]));
-      display.print((char)247);
-      display.print("C");
     }
   }
   display.display();
